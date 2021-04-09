b73523238f56854f43ed2fff5fe49c34a3bc8e3c
Sidney Avila <savila1212@gmail.com>
2021-04-09 11:52:50 -0500

    added spring boot file

---
+++ src/main/java/com/worldtravel/demo/WorldTravelProjectApplication.java
@@ -0,0 +1,13 @@
+package com.worldtravel.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldTravelProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldTravelProjectApplication.class, args);
+    }
+
+}
