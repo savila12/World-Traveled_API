aadc91c067da6da268446088bf36ccc8e2547f59
Sidney Avila <savila1212@gmail.com>
2021-04-09 12:07:48 -0500

    test

---
+++ src/test/java/com/worldtravel/demo/WorledTraveledApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldtravel.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorledTraveledApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
