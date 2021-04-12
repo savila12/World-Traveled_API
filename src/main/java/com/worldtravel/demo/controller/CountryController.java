57c3bb9829bdc83a84ecba75bca4d5abf63f9cdf
Sidney Avila <savila1212@gmail.com>
2021-04-12 14:38:12 -0500

    added get mapping for a list of countries

--- src/main/java/com/worldtravel/demo/controller/CountryController.java
+++ src/main/java/com/worldtravel/demo/controller/CountryController.java
@@ -1,11 +1,14 @@
 package com.worldtravel.demo.controller;
 
+import com.worldtravel.demo.model.Country;
 import com.worldtravel.demo.repository.CountryRepository;
 import com.worldtravel.demo.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api")
@@ -21,5 +24,12 @@ public class CountryController {
     @GetMapping("/hello")
     public String helloWorld(){
         return "Hello World";
+    }
+
+    //http://localhost:PORTNUMBER/api/categories
+    @GetMapping("/countries")
+    public List<Country> getCountries (){
+        System.out.println("calling getCountries =====>");
+        return countryService.getCountries();
     }
 }
