791d940135cbaa71907e4b512d41da9ff7aeb44b
Sidney Avila <savila1212@gmail.com>
2021-04-12 10:03:58 -0500

    old file

--- src/main/java/com/worldtravel/demo/model/User.java
+++ src/main/java/com/worldtravel/demo/model/User.java
@@ -1,4 +1,23 @@
 package com.worldtravel.demo.model;
 
+
+import javax.persistence.*;
+
+@Entity
 public class User {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String userName;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
 }
