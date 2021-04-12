bb2c330d73f82331a22e73618c03257a40f6c693
Sidney Avila <savila1212@gmail.com>
2021-04-12 10:35:03 -0500

    update adventure class

--- src/main/java/com/worldtravel/demo/model/Adventure.java
+++ src/main/java/com/worldtravel/demo/model/Adventure.java
@@ -1,5 +1,73 @@
 package com.worldtravel.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "adventures")
 public class Adventure {
 
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String dateWent;
+
+    @Column
+    private String adventureDescription;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    public Adventure() {
+    }
+
+    public Adventure(Long id, String dateWent, String adventureDescription) {
+        this.id = id;
+        this.dateWent = dateWent;
+        this.adventureDescription = adventureDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDateWent() {
+        return dateWent;
+    }
+
+    public void setDateWent(String dateWent) {
+        this.dateWent = dateWent;
+    }
+
+    public String getAdventureDescription() {
+        return adventureDescription;
+    }
+
+    public void setAdventureDescription(String adventureDescription) {
+        this.adventureDescription = adventureDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "Adventure{" +
+                "id=" + id +
+                ", dateWent='" + dateWent + '\'' +
+                ", adventureDescription='" + adventureDescription + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/worldtravel/demo/model/Country.java
+++ src/main/java/com/worldtravel/demo/model/Country.java
@@ -1,4 +1,50 @@
 package com.worldtravel.demo.model;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "countries")
 public class Country {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+
+    public Country() {
+    }
+
+    public Country(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
