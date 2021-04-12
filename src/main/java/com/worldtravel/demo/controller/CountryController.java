0aa7c3933aecfe412bf7b6c349c3b92a615a8edd
Sidney Avila <savila1212@gmail.com>
2021-04-12 14:25:54 -0500

    added get mapping hello world for testing country controller

--- src/main/java/com/worldtravel/demo/controller/CountryController.java
+++ src/main/java/com/worldtravel/demo/controller/CountryController.java
@@ -2,13 +2,24 @@ package com.worldtravel.demo.controller;
 
 import com.worldtravel.demo.repository.CountryRepository;
 import com.worldtravel.demo.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api")
 public class CountryController {
-    private CountryRepository countryRepository;
 
     private CountryService countryService;
+
+    @Autowired
+    public void setCountryService(CountryService countryService){
+        this.countryService = countryService;
+    }
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello World";
+    }
 }
