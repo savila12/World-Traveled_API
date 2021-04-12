306462b363974370bcffc42e943bc1d26485a4e7
Sidney Avila <savila1212@gmail.com>
2021-04-12 14:11:44 -0500

    autowired authentication Manager

--- src/main/java/com/worldtravel/demo/service/UserService.java
+++ src/main/java/com/worldtravel/demo/service/UserService.java
@@ -7,6 +7,7 @@ import com.worldtravel.demo.model.loginRequest.LoginRequest;
 import com.worldtravel.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -20,6 +21,9 @@ public class UserService {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
