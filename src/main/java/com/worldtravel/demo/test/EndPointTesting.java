9fc3b988c9fd1ffcfdc933cbb6911886f6f0be17
Sidney Avila <savila1212@gmail.com>
2021-04-14 11:03:31 -0500

    added JUNIT Spring test class

---
+++ src/main/java/com/worldtravel/demo/test/EndPointTesting.java
@@ -0,0 +1,13 @@
+package com.worldtravel.demo.test;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EndPointTesting {
+
+    @Test
+    public void contextLoads(){}
+    
+
+}
