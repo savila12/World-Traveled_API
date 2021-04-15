8da0c831218f6237b197b4f7bbc582239225dd04
Sidney Avila <savila1212@gmail.com>
2021-04-15 15:26:02 -0500

    working on Login User test

--- src/test/java/com/worldtravel/demo/controller/UserControllerTest.java
+++ src/test/java/com/worldtravel/demo/controller/UserControllerTest.java
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.worldtravel.demo.model.User;
+import com.worldtravel.demo.model.loginRequest.LoginRequest;
+import com.worldtravel.demo.security.MyUserDetails;
+import com.worldtravel.demo.security.WithMockCustomUser;
+import com.worldtravel.demo.service.AdventureService;
 import com.worldtravel.demo.service.UserService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,11 +18,24 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.json.JsonParseException;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
 import java.awt.*;
 import java.io.IOException;
 import java.util.List;
@@ -28,15 +45,20 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
+//@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
-    @Mock
+    @MockBean
     private UserService userService;
 
     private User user;
 
     @InjectMocks
     private UserController userController;
+
+    @InjectMocks
+    private AdventureService adventureService;
 
     @Autowired
     private MockMvc mockMvc;
@@ -70,14 +92,19 @@ class UserControllerTest {
                 andExpect(MockMvcResultMatchers.status().isOk());
     }
 
+
     @Test
-    void loginUser() {
+    @WithMockUser(username = "jslkl@sl.com", password = "123456")
+    void loginUser() throws Exception{
+        mockMvc.perform(post("/auth/users/login")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
     @Test
     void updatePassword() {
     }
-    
+
     @Test
     void updateEmail() {
     }
