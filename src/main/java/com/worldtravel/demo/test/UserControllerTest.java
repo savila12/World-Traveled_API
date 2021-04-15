58eac1b577ff129dd55fb277aaa15bba77cfab3c
Sidney Avila <savila1212@gmail.com>
2021-04-15 11:05:43 -0500

    successfull error on Post createUser test

--- src/main/java/com/worldtravel/demo/test/UserControllerTest.java
+++ src/main/java/com/worldtravel/demo/test/UserControllerTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.worldtravel.demo.WorledTraveledApiApplication;
 import com.worldtravel.demo.controller.UserController;
 import com.worldtravel.demo.model.User;
 import com.worldtravel.demo.service.UserService;
@@ -15,11 +16,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -28,6 +35,7 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
 
 import java.io.IOException;
 import java.util.List;
@@ -40,12 +48,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 //@ExtendWith(MockitoExtension.class)
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = UserController.class)
-@WebAppConfiguration
+@SpringBootTest(classes = WorledTraveledApiApplication.class)
+//@WebMvcTest(value = UserController.class)
+//@ContextConfiguration
+//@WebAppConfiguration
 public class UserControllerTest {
 
-//    @Mock
-//    private UserService userService;
+    @MockBean
+    private UserService userService;
+
+    String exampleUserCreation = "{\"userName\": \"Sid\", \"email \": \"sldl@sls.com\", \"password\": \"password\" }";
 //    private User user;
 //    private List<User> userList;
 //
@@ -53,14 +65,18 @@ public class UserControllerTest {
 //    @InjectMocks
 //    private UserController userController;
 
+
     @Autowired
     WebApplicationContext webApplicationContext;
 
+    @Autowired
     private MockMvc mockMvc;
 
     @BeforeEach
     public void setUp(){
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .build();
     }
 
     //convert java object to Json String
@@ -82,16 +98,23 @@ public class UserControllerTest {
 
     @Test
     public void PostMappingOfUser()throws Exception{
-        User user = new User();
-        user.setId(3L);
-        user.setUserName("Sid");
-        user.setEmail("sdfdfd@sl.com");
-        user.setPassword("password");
+        User mockUser = new User(3L, "Sid", "elsl@gm.com", "password");
+        Mockito.when(userService.createUser(Mockito.any(User.class))).thenReturn(mockUser);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/auth/users/register")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(exampleUserCreation)
+                .contentType(MediaType.APPLICATION_JSON);
 
-        String inputJson = mapToJson(user);
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/auth/users/register").contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
-        int status = mvcResult.getResponse().getStatus();
-        assertEquals(201, status);
+        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+
+        MockHttpServletResponse response = mvcResult.getResponse();
+        assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+
+       // String inputJson = mapToJson(user);
+       // MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/register").contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
+        //int status = mvcResult.getResponse().getStatus();
+        //assertEquals(201, status);
 //        String content = mvcResult.getResponse().getContentAsString();
 //        assertEquals(content, "Product is created successfully");
         //when(userService.createUser(any())).thenReturn(user);
