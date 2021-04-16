316f58953d670474733a609cf3501a7e39faeecd
Sidney Avila <savila1212@gmail.com>
2021-04-15 19:37:12 -0500

    setup user Profile

--- src/test/java/com/worldtravel/demo/controller/UserProfileControllerTest.java
+++ src/test/java/com/worldtravel/demo/controller/UserProfileControllerTest.java
@@ -1,4 +1,62 @@
 package com.worldtravel.demo.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.worldtravel.demo.model.User;
+import com.worldtravel.demo.model.UserProfile;
+import com.worldtravel.demo.security.MyUserDetailsService;
+import com.worldtravel.demo.service.UserService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringJUnit4ClassRunner.class)
 public class UserProfileControllerTest {
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    MyUserDetailsService myUserDetailsService;
+
+    private UserProfile userProfile;
+
+    @InjectMocks
+    private UserProfileController userProfileController;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    public void setUp(){
+        userProfile = new UserProfile(2L, "Sidney", "Avila", "My profile");
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @AfterEach
+    public void tearDown(){
+        userProfile = null;
+    }
+
+    private String mapToJson(Object object) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(object);
+    }
+
+
+
 }
