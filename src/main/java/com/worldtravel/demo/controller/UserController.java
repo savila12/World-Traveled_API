e356f3b5d1438a5b6b376ac04ec22b8d5081ea1a
Sidney Avila <savila1212@gmail.com>
2021-05-11 12:01:41 -0500

    commented out @CrossOrigins

--- src/main/java/com/worldtravel/demo/controller/UserController.java
+++ src/main/java/com/worldtravel/demo/controller/UserController.java
@@ -19,7 +19,7 @@ public class UserController {
     }
 
     //http://localhost:9092/auth/users/register
-    @CrossOrigin
+    //@CrossOrigin
     @PostMapping("/register")
     public User createUser(@RequestBody User userObject){
         System.out.println("calling createUser");
